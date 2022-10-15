a09356e2d6ad0ceed35683ec5d7f80d42a87fa7d
Aadarsh <kumaraadarsh17@gmail.com>
2022-10-15 19:36:18 +0530

    created starter project

---
+++ src/test/java/com/group_c/velo/VeloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group_c.velo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
