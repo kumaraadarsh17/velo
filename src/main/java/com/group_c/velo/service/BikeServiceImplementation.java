5d95cacab65be66a2919f9af17fe9c53b9ab0f4b
Aadarsh <kumaraadarsh17@gmail.com>
2022-10-16 21:57:30 +0530

    implemented bike service

--- src/main/java/com/group_c/velo/service/BikeServiceImplementation.java
+++ src/main/java/com/group_c/velo/service/BikeServiceImplementation.java
@@ -1,28 +1,38 @@
 package com.group_c.velo.service;
 
 import com.group_c.velo.entity.Bike;
+import com.group_c.velo.repository.BikeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
+@AllArgsConstructor
 public class BikeServiceImplementation implements BikeService{
+    BikeRepository bikeRepository;
 
     @Override
     public Bike getBike(Long id) {
-        return null;
+        Optional<Bike> bike = bikeRepository.findById(id);
+        if(bike.isPresent()){
+            return bike.get();
+        }   else return null; //TODO: create an exception for bike not found
     }
 
     @Override
     public Bike saveBike(Bike bike) {
-        return null;
+        return bikeRepository.save(bike);
     }
 
     @Override
     public void DeleteBike(Long id) {
-
+        bikeRepository.deleteById(id);
     }
 
     @Override
     public List<Bike> getAllBikes() {
-        return null;
+        return (List<Bike>) bikeRepository.findAll();
     }
 }
